c51dc6ef25492a4441d7db3cffef358462082e0e
Brandon Palmeros <brandonpaldom@gmail.com>
2022-07-19 12:24:28 -0500

    Improvements in login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -17,11 +17,37 @@ export function LoginPage() {
 
   return (
     <div className={styles.background}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Login page</h1>
-        <button onClick={onLogin} className={styles.button}>
-          Login
-        </button>
+      <div className={`${styles.container} animate__animated animate__fadeIn`}>
+        <div className={styles.avatar} />
+        <h1 className={styles.title}>Sign in yo your account</h1>
+        <p className={styles.subtitle}>Log in with the test user</p>
+        <div className={styles.form}>
+          <label className={styles['input-container']}>
+            <span className={styles.label}>Email address</span>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className={styles.input}
+              value="brandon"
+              readOnly
+            />
+          </label>
+          <label className={styles['input-container']}>
+            <span className={styles.label}>Password</span>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className={styles.input}
+              value="brandon"
+              readOnly
+            />
+          </label>
+          <button onClick={onLogin} className={styles.button}>
+            Sign in
+          </button>
+        </div>
       </div>
     </div>
   );
